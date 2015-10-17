c73ce9ef48ea2566d851279261bfa645f6309ffd
evanbaumgardner <ebaumgardner@gmail.com>
2015-10-17 10:23:30 -0700

    Initial Commit

---
+++ DycksSpecial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DycksSpecial
+//
+//  Created by Evan Baumgardner on 10/17/15.
+//  Copyright © 2015 Evan Baumgardner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
