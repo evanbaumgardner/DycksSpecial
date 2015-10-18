8ef2e88bfc553bb7a74fd1dddc170dd1335312a0
evanbaumgardner <ebaumgardner@gmail.com>
2015-10-17 17:20:03 -0700

    first commit

---
+++ DycksSpecial/SlideMenuViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SlideMenuController.swift
+//  DycksSpecial
+//
+//  Created by Evan Baumgardner on 10/17/15.
+//  Copyright © 2015 Evan Baumgardner. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {}
+
